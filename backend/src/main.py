556d32bec9c0695f15f8184a49f7754e49e33d7d
N1ck26 <nn.molchanov26@mail.ru>
2024-10-04 00:44:11 +0300

    ((((

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from src.router import router as cmc_router
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 
 app = FastAPI()
 app.include_router(cmc_router)
@@ -21,6 +20,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
