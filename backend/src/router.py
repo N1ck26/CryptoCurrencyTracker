cff55a7d1bba29a5c607b9f9d5b0abeffbed5d8b
N1ck26 <nn.molchanov26@mail.ru>
2024-10-10 10:51:39 +0300

    fix: api router

--- backend/src/router.py
+++ backend/src/router.py
@@ -3,11 +3,11 @@ from src.init_cmc import cmc_client
 
 # router = APIRouter(prefix='/cryptocurrencies')
 router = APIRouter()
-@router.get('')
+@router.get('/cryptocurrencies')
 async def get_all_cryptocurrencies():
     return await cmc_client.get_listings()
 
 
-@router.get('/{currency_id}')
+@router.get('/cryptocurrencies/{currency_id}')
 async def get_cryptocurrency(currency_id: int):
     return await cmc_client.get_quote(currency_id)
